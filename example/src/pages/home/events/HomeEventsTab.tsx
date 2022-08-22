c0858b8bd128617203264daa7b653d4081e6ac32
Andre Tortolano <andret@appcues.com>
2022-08-22 10:33:50 -0400

    ♻️  Updated IONIC example to match native samples + android capacitor

--- example/src/pages/home/events/HomeEventsTab.tsx
+++ example/src/pages/home/events/HomeEventsTab.tsx
@@ -1,27 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import './Tab1.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons } from '@ionic/react';
+import './HomeEventsTab.css';
 import { Appcues } from 'appcues-capacitor';
 
-const Tab1: React.FC = () => {
+const HomeEventsTab: React.FC = () => {
+  const openDebugger = () => {
+    Appcues.debug()
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Appcues Ionic</IonTitle>
+        <IonButtons slot="secondary">
+           <IonButton onClick={openDebugger} color=''>DEBUG</IonButton>
+        </IonButtons>
+          <IonTitle>Trigger Events</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Appcues Ionic</IonTitle>
+            <IonTitle size="large">Trigger Events</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonButton expand="block" onClick={() => Appcues.track({name: "event1"})} >Trigger Event 1</IonButton>
         <IonButton expand="block" onClick={() => Appcues.track({name: "event2"})} >Trigger Event 2</IonButton>
-        <IonButton expand="block" onClick={() => Appcues.debug()} >Open debug</IonButton>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default HomeEventsTab;
