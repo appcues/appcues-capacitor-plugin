c06b8d4cb884a516d4d718c286e03a2438152ea0
andretortolano <andre@appcues.com>
2024-05-30 16:14:54 -0400

    🔧 Configure example notification extension build

---
+++ example/ios/App/AppcuesNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,11 @@
+//
+//  NotificationService.swift
+//  AppcuesNotificationServiceExtension
+//
+//  Created by Andre Tortolano on 30/05/24.
+//
+
+import UserNotifications
+import AppcuesNotificationService
+
+class NotificationService: AppcuesNotificationServiceExtension {}
