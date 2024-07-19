f10b30e9a7332a2b0139630d0c5933a5c24b3aad
andretortolano <andre@appcues.com>
2024-07-19 11:45:28 -0400

    ⬆️ Update to SDK 3.3.0

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -4,7 +4,12 @@ const config: CapacitorConfig = {
   appId: 'com.appcues.samples.ionic',
   appName: 'appcues-ionic-app',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  
+  ios: {
+    // Adding schema name so "npx cap run ios" knows what to look for
+    scheme: "App",
+  },
 };
 
 export default config;
